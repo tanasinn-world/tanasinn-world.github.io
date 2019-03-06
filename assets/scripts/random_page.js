ed4157d879f9d999062b8a3cd626bb7b708acaf8
mizunototori <mizunototori@gmail.com>
2019-03-06 23:32:50 +0900

    Modified random selection of page

--- assets/scripts/random_page.js
+++ assets/scripts/random_page.js
@@ -1,5 +1,13 @@
 function random_page(){
 
-const u =  Array.from(new Array(6)).map((v, i) => '/posts/' + i.toString() + '.html') 
-location.href=(u[Math.floor(Math.random()*u.length)]);
+const c_href = location.href
+const c = Number(c_href.split('/')[c_href.split('/').length -1].replace(/\.html/, ''));
+
+const u =  Array.from(new Array(6)).map((v, i) => '/posts/' + i.toString() + '.html');
+const removed = u.splice(c, 1);
+
+const next_u = (u[Math.floor(Math.random()*u.length)]);
+
+location.href = next_u
+
 }
