a7ade4d5541d95f558771c866a926e2450f21cf1
mizunototori <mizunototori@gmail.com>
2019-03-06 23:35:35 +0900

    Fixed number of pages

--- assets/scripts/random_page.js
+++ assets/scripts/random_page.js
@@ -3,7 +3,7 @@ function random_page(){
 const c_href = location.href
 const c = Number(c_href.split('/')[c_href.split('/').length -1].replace(/\.html/, ''));
 
-const u =  Array.from(new Array(6)).map((v, i) => '/posts/' + i.toString() + '.html');
+const u =  Array.from(new Array(8)).map((v, i) => '/posts/' + i.toString() + '.html');
 const removed = u.splice(c, 1);
 
 const next_u = (u[Math.floor(Math.random()*u.length)]);
